ab899c7690ff1ab30d8f76d9b85fc7beb72e8d1f
mcarton <cartonmartin+git@gmail.com>
2016-10-09 21:04:16 +0200

    Fix a test that broke on a recent nightly

--- tests/compile-fail/arity-2.rs
+++ tests/compile-fail/arity-2.rs
@@ -2,8 +2,8 @@
 #![plugin(plague)]
 
 plague! {
-    for [ //~ERROR: this function takes 0 parameters but 2 parameters were supplied
-        (1, 2),
+    for [
+        (1, 2), //~ERROR: this function takes 0 parameters but 2 parameters were supplied
     ]
     test fn foo() {
     }
